ffe7ac1a6dba3b7c11ed19018f167a67467f3344
foomi <foomi.business@gmail.com>
2023-10-13 19:17:14 -0500

    starting fresh with better performance

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [comments, setComments] = useState([]);
-  const [username, setUsername] = useState('');
-  const [text, setText] = useState('');
-
-  const addComment = () => {
-    if (username && text) {
-      setComments([...comments, { username, text }]);
-      setUsername('');
-      setText('');
-    }
-  };
-
   return (
     <div className="App">
-      <h1>KATE3O</h1>
-      <div className="comment-form">
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Write your comment..."
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button onClick={addComment}>Submit</button>
-      </div>
-      <div className="comments">
-        {comments.map((comment, index) => (
-          <div key={index} className="comment">
-            <strong>{comment.username}:</strong> {comment.text}
-          </div>
-        ))}
-      </div>
+      <h1 className="title">KATE3O</h1>
     </div>
   );
 }
